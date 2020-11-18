82327c742ab875d051107f447f9896fbb439ffcc
Aman Dhiman <amandhiman251@gmail.com>
2020-11-18 23:40:21 +0530

    setting global configaration baseURL,headers common/post/get index.js

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -10,7 +10,7 @@ class FullPost extends Component {
         if(this.props.id)
         {
             if(!this.state.loadedPost || (this.state.loadedPost.id !== this.props.id )){
-                axios.get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+                axios.get("/posts/" + this.props.id)
                 .then(response => {
                 this.setState({loadedPost: response.data})
                 //console.log(response)
@@ -19,7 +19,7 @@ class FullPost extends Component {
         }
     }
     deleteDataHandler = () => {
-        axios.delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+        axios.delete("/posts/" + this.props.id)
         .then(response => {
             console.log(response)
         });
