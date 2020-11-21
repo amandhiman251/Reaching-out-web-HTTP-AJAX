591e1fa38a9803e3190f8b24824a9281f2bc7652
Aman Dhiman <amandhiman251@gmail.com>
2020-11-21 20:57:27 +0530

    transferring router props to embeded component using withRouter in post.js

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import { withRouter } from 'react-router-dom';
 import './Post.css';
 
-const post = (props) => (
+const post = (props) => {
+    console.log(props);
+    return (
     <article className="Post" onClick={props.clicked}>
         <h1>{props.title}</h1>
         <div className="Info">
@@ -10,5 +12,6 @@ const post = (props) => (
         </div>
     </article>
 );
+    }
 
-export default post;
+export default withRouter(post);
