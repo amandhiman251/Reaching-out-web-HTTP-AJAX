f583fb1067b8e18967c0650c6c5a5a21b3e0b18e
Aman Dhiman <amandhiman251@gmail.com>
2020-11-22 00:35:13 +0530

    displaying full post without routing props programatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from '../../../../src/axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
-import {Link} from 'react-router-dom';
 
 
 class Posts extends Component {
@@ -30,20 +29,22 @@ class Posts extends Component {
     }
 
     postSelectedHandler= (id) => {
-        this.setState({selectedPostId: id});
+        //this.props.history.push('/'+id);
+        this.props.history.push({pathname:'/'+ id})
     }
 
     render() {
         let posts = <p>Something went wrong on backend!</p>
         if(!this.state.error){
             posts = this.state.posts.map(post => {
-            return <Link key={post.id} to={'/' + post.id} >
+            return (//<Link key={post.id} to={'/' + post.id} >
                 <Post  
                 title ={post.title} 
                 author={post.author}
                 clicked={()=> this.postSelectedHandler(post.id)}
             />
-            </Link>
+            //</Link>
+            );
         });
     }
         return(
