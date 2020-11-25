395c235a6df75499d5c5fb970ed4da810535e934
Aman Dhiman <amandhiman251@gmail.com>
2020-11-25 15:52:46 +0530

    redirecting using Redirect from '/' to '/posts'

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 //import axios from 'axios';
 import Posts from './Posts/Posts';
 import './Blog.css';
-import {Route, NavLink, Switch} from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 import NewPost from '../Blog/NewPost/NewPost';
 
 class Blog extends Component {
@@ -33,7 +33,8 @@ class Blog extends Component {
                 <Switch>
                     <Route path='/new-post' component={NewPost} />
                     <Route path='/posts' component={Posts} />
-                    <Route path='/' component={Posts} /> {/*  displaying the same page on diff routes */}
+                    <Redirect from='/' to='/posts' />
+                    {/* <Route path='/' component={Posts} /> redirect/displaying the same page on diff routes */}
                 </Switch>
             </div>
         );
