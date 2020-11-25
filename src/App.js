9b8a954fc459f25a7bdf9142b2566e80b017f4a8
Aman Dhiman <amandhiman251@gmail.com>
2020-11-25 19:45:20 +0530

    configure server to always return index.html for each 404 error or other requests

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter basename='/something'> In case if your root folder is in other directory add /foldername to set base url 
       <BrowserRouter>
         <div className="App">
           <Blog />
